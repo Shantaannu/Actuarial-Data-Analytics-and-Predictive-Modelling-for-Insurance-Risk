6317d5db5ab8ddcc1f6fa7a537db7c9960b920bc
Shantaannu <Shantanududhankar670@gmail.com>
2026-09-03 10:44:58 +0100

    Updating Code

---
+++ Scripts/data_cleaning.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+def clean_data(df):
+    
---
+++ packages/data_import.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df_claims = pd.read_csv("../data/raw/ncid_private_motor_ultimate_claims.csv")
+df_claims_premium = pd.read_csv("../data/raw/ncid_private_motor_ultimate_prem_claims.csv")
